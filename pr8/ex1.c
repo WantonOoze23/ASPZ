131fb119bd83cabe9f128ab6e875cc6bd07c0fce
Daniil <mine16978@gmail.com>
2025-05-16 19:06:37 +0300

    ex1 pr8 fixes

--- pr8/ex1.c
+++ pr8/ex1.c
@@ -1,44 +1,28 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <errno.h>
 
 int main() {
-    int fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) {
-        perror("open");
-        return 1;
-    }
-
-    char buffer[] = "Hello, World!";
-    ssize_t nbytes = strlen(buffer);
-    ssize_t count = write(fd, buffer, nbytes);
-
-    if (count == -1) {
-        perror("write");
-    } else {
-        printf("Requested %ld bytes, wrote %ld bytes\n", nbytes, count);
-    }
-
     int pipefd[2];
+    char buffer[4096];
+    memset(buffer, 'A', sizeof(buffer));
+
     if (pipe(pipefd) == -1) {
         perror("pipe");
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
-    char large_buffer[100000];
-    nbytes = sizeof(large_buffer);
-    count = write(pipefd[1], large_buffer, nbytes);
-
-    if (count == -1) {
-        perror("write to pipe");
-    } else {
-        printf("Requested %ld bytes, wrote %ld bytes to pipe\n", nbytes, count);
-    }
-
-    close(fd);
     close(pipefd[0]);
+
+    ssize_t count = write(pipefd[1], buffer, sizeof(buffer));
+    if (count < 0) {
+        perror("write");
+    } else {
+        printf("write() записав %zd байт із %zu\n", count, sizeof(buffer));
+    }
+
     close(pipefd[1]);
     return 0;
 }
