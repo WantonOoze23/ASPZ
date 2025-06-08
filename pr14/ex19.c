bcde0fef89c011d7daf12769ec25f789dffc14b3
Daniil <mine16978@gmail.com>
2025-06-09 00:08:15 +0300

    Refactor code structure for improved readability and maintainability

--- pr14/ex19.c
+++ pr14/ex19.c
@@ -10,7 +10,9 @@ volatile int ticks_handled = 0;
 volatile int total_lost_ticks = 0;
 
 void handler(int sig, siginfo_t *si, void *uc) {
-    int overrun = timer_getoverrun((timer_t)si->si_value.sival_ptr);
+    timer_t *tidp = si->si_value.sival_ptr;
+    int overrun = timer_getoverrun(*tidp);
+
     if (overrun == -1) {
         perror("timer_getoverrun");
     } else {
@@ -19,7 +21,6 @@ void handler(int sig, siginfo_t *si, void *uc) {
 
     ticks_handled++;
 
-    // Імітація навантаження (штучна затримка)
     for (volatile int i = 0; i < 100000000; ++i);
 
     printf("Tick %d, Overrun: %d, Total lost: %d\n", ticks_handled, overrun, total_lost_ticks);
@@ -30,14 +31,18 @@ int main() {
     memset(&sa, 0, sizeof(sa));
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = handler;
-    sigaction(SIGRTMIN, &sa, NULL);
+
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
     timer_t timerid;
     struct sigevent sev;
     memset(&sev, 0, sizeof(sev));
     sev.sigev_notify = SIGEV_SIGNAL;
     sev.sigev_signo = SIGRTMIN;
-    sev.sigev_value.sival_ptr = &timerid;
+    sev.sigev_value.sival_ptr = &timerid; 
 
     if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1) {
         perror("timer_create");
@@ -46,9 +51,9 @@ int main() {
 
     struct itimerspec its;
     its.it_value.tv_sec = 0;
-    its.it_value.tv_nsec = 10000000; // перший тік через 10 мс
+    its.it_value.tv_nsec = 10000000;      // перший тік через 10 мс
     its.it_interval.tv_sec = 0;
-    its.it_interval.tv_nsec = 10000000; // кожні 10 мс
+    its.it_interval.tv_nsec = 10000000;   // інтервал 10 мс
 
     if (timer_settime(timerid, 0, &its, NULL) == -1) {
         perror("timer_settime");
@@ -58,7 +63,7 @@ int main() {
     // Працюємо 5 секунд
     sleep(5);
 
-    printf("Handled ticks: %d, Total lost ticks: %d\n", ticks_handled, total_lost_ticks);
+    printf("FINISHED: Ticks handled = %d, Total lost ticks = %d\n", ticks_handled, total_lost_ticks);
 
     return 0;
 }
